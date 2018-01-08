867450432cb5e4250c97ee9d9951bbbc048335f4
Hayden Jamieson <haydenjamieson8@gmail.com>
2018-01-08 15:47:10 +0000

    Setup save function

--- GoalPost-app/Controller/FinishGoalVC.swift
+++ GoalPost-app/Controller/FinishGoalVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import CoreData
+
+let appDelegate = UIApplication.shared.delegate as? AppDelegate
 
 class FinishGoalVC: UIViewController, UITextFieldDelegate {
 
@@ -23,11 +26,45 @@ class FinishGoalVC: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+       // createGoalBtn.translatesAutoresizingMaskIntoConstraints = true
         createGoalBtn.bindToKeyboard()
         pointsTextField.delegate = self
     }
 
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismissDetail()
+    }
+    
     @IBAction func createGoalBtnWasPressed(_ sender: Any) {
+        if pointsTextField.text != nil {
+            save { (complete) in
+                if complete {
+                    print("saved")
+                    performSegue(withIdentifier: SHOW_GOALS_VC, sender: self)
+                }
+            }
+        }
+    }
+    
+    func save(completion: (_ finished: Bool) -> () ) {
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        let goal = Goal(context: managedContext)
+        goal.goalDescription = goalDescription
+        goal.goalType = goalType.rawValue
+        goal.goalCompletionValue = Int32(pointsTextField.text!)!
+        goal.goalProgress = Int32(0)
+        
+        do {
+          try managedContext.save()
+            completion(true)
+        } catch {
+            debugPrint("Could not save \(error.localizedDescription)")
+            completion(false)
+        }
         
     }
+    
+    
+    
+    
 }
