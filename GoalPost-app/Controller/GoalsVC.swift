d911d435e705dbed2238531b92ce5ecb6664c327
Hayden Jamieson <haydenjamieson8@gmail.com>
2018-01-08 10:52:54 +0000

    GoalVC Setup
    Table view and cell class configured
    UI and table view cell contents configured

--- GoalPost-app/Controller/GoalsVC.swift
+++ GoalPost-app/Controller/GoalsVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GoalsVC.swift
 //  GoalPost-app
 //
 //  Created by Hayden Jamieson on 08/01/2018.
@@ -8,17 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class GoalsVC: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func addGoalBtnPressed(_ sender: Any) {
+        
     }
+    
 
 
 }
---
+++ GoalPost-app/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GoalPost-app
+//
+//  Created by Hayden Jamieson on 08/01/2018.
+//  Copyright © 2018 Hayden Jamieson. All rights reserved.
+//
+
+import Foundation
+
+
+let GOAL_TABLE_CELL: String = "goalCell"
